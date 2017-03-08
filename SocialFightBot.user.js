20ab67311fb61c2316b1e83eb401ca5a5a995c1b
Mads Mogensen <lort256c@gmail.com>
2017-03-08 16:37:21 +0100

    Initial commit
    Added file

---
+++ SocialFightBot.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         SocialFightBot
+// @namespace    https://github.com/mads256c/SocialFightBot
+// @version      0.1
+// @description  try to take over the world!
+// @author       DaaseAllan and mads256c
+// @match        http://socialfight.dk
+// @grant        none
+// ==/UserScript==
+
+(function() {
+
+})();
