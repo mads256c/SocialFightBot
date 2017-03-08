3fc83460d566baf63b331447e5e7f2b752cb5f76
Mads Mogensen <lort256c@gmail.com>
2017-03-08 18:43:57 +0100

    Set version 0.1 on levelup

--- SocialFightBot-LevelUp.user.js
+++ SocialFightBot-LevelUp.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         SocialFightBot-LevelUp
 // @namespace    https://github.com/mads256c/SocialFightBot
-// @version      0.3
+// @version      0.1
 // @description  A bot for socialfight.dk
 // @author       DaaseAllan and mads256c
-// @match        http://socialfight.dk/fight
+// @match        http://socialfight.dk/levelup
 // ==/UserScript==
 
 (function() {
