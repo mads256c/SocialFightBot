f049cdf841bc6a19dee72f7d29b0101d6d5a4f09
Mads Mogensen <lort256c@gmail.com>
2017-03-08 18:05:30 +0100

    Added basic functionallity

--- SocialFightBot.user.js
+++ SocialFightBot.user.js
@@ -4,37 +4,44 @@
 // @version      0.1
 // @description  A bot for socialfight.dk
 // @author       DaaseAllan and mads256c
-// @match        http://socialfight.dk/*
-// @grant        none
+// @match        http://socialfight.dk/fight
+// @grant        GM_setValue
+// @grant        GM_getValue
 // ==/UserScript==
 
-
 (function() {
-    var PlayerHealth = 0;
-    var PlayerHealthMax = 0;
-    
     //Get all the html things.
     var HTMLAttack = document.getElementById("attack1");
     var HTMLHealth = document.getElementById("health");
     var HTMLModal = document.getElementById("myModal2");
-    
+
     var HTMLHealthString = HTMLHealth.innerHTML.replace(/\s+/, "");
     var split1 = HTMLHealthString.split("/");
     var split2 = split1[1].split("HP");
-    
-    PlayerHealth = split1[0];
-    PlayerHealthMax = split2[0];
-    
-    
-    console.log(split1[0] + " " + split2[0]);
-    
-    console.log(HTMLHealth.innerHTML);
-    console.log(HTMLModal.style.display);
-    
-    if (HTMLModal.style.display == "block")
+
+    var PlayerHealth = split1[0];
+    var PlayerHealthMax = split2[0];
+    var PlayerHealthMin = PlayerHealth / 4;
+
+    console.log(GM_getValue("isRegenerating", false));
+
+    if (PlayerHealth <= PlayerHealthMin)
     {
-        //
+        GM_setValue("isRegenerating", true);
     }
-        
-    
+
+    if (PlayerHealth => PlayerHealthMax)
+    {
+        GM_setValue("isRegenerating", false);
+    }
+
+    if (!GM_getValue("isRegenerating", false))
+    {
+        document.getElementById("attack1").click();
+    }
+
+
+        setTimeout(function () {
+            location.reload();
+    }, 30 * 1000);
 })();
