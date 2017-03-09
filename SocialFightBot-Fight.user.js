0dc4fbdd375d3cd50c6746f5382f7c849d08ccc0
Mads Mogensen <lort256c@gmail.com>
2017-03-09 18:16:06 +0100

    Added better logging and moved things
    I also commented a lot of things, moved some code and optimized the
    script.

--- SocialFightBot-Fight.user.js
+++ SocialFightBot-Fight.user.js
@@ -1,75 +1,105 @@
 // ==UserScript==
 // @name         SocialFightBot-Fight
 // @namespace    https://github.com/mads256c/SocialFightBot
-// @version      0.7
+// @version      0.9
 // @description  A bot for socialfight.dk
 // @author       DaaseAllan and mads256c
-// @match        http://socialfight.dk/fight
+// @match        http*://socialfight.dk/fight
 // @run-at       document-end
 // ==/UserScript==
 
-(function() {
-    var isRegenerating = false;
-    const responses = ["Vind en gratis pik i røven, sendes over MobilePay.",
-                       "Tak fordi du bruger vores bot!",
-                       "Hættedregne represent!",
-                       "BAZINGA!",
-                       "Quality comedy!",
-                       "Strip for os!",
-                       "If a women has starch masks on her body does that mean she has been pargnet before.?",
-                       "My friends did a luigi board.. and it mentioned me! PLEAS HELP!?",
-                       "YEE",
-                       "Vidste du at vi kan skrive lige hvad vi vil i denne boks?",
-                       "hvad vis du vis hvad han har gjort så ville du os forstå det 👌"];
+//*************
+//* Constants *
+//*************
+//The HTML class or id names. If the site changes the names we can easily change it here.
+const HTMLAttack = "attack1";
+const HTMLHealth = "health";
+const HTMLAlert = "alert";
 
-    //The HTML constants, so they can be updated if the site changes.
-    const HTMLAttack = "attack1";
-    const HTMLAttackDiv = "normalattack";
-    const HTMLHealth = "health";
-    const HTMLAlert = "alert";
+//The site killed the alert, so we are adding it back in here:
+const AlertHTML = "<a class=\"alert\" href=\"#\">Shitty Alert!</a>";
+
+//Stupid peices of text that we display in the "alert" bar.
+const responses = ["Vind en gratis pik i røven, sendes over MobilePay.",
+                   "Tak fordi du bruger vores bot!",
+                   "Hættedregne represent!",
+                   "BAZINGA!",
+                   "Quality comedy!",
+                   "If a women has starch masks on her body does that mean she has been pargnet before.?",
+                   "My friends did a luigi board.. and it mentioned me! PLEAS HELP!?",
+                   "YEE",
+                   "Vidste du at vi kan skrive lige hvad vi vil i denne boks?",
+                   "hvad vis du vis hvad han har gjort så ville du os forstå det 👌",
+                   "Lavet af DaaseAllan og mads256c"];
+
+//**************************************
+//* Code for the logging functionality *
+//**************************************
+var logString = "<h2 style=\"font-size: 1.5em\">Console Log</h2>";
+
+function log(string)
+{
+    console.log(string);
+    logString += "<p style=\"color: AliceBlue\">" + string + "</p>";
+}
+
+function warn(string)
+{
+    console.warn(string);
+    logString += "<p style=\"color: DarkOrange\">" + string + "</p>";
+}
+
+function error(string)
+{
+    console.error(string);
+    logString += "<p style=\"color: DarkRed\">" + string + "</p>";
+}
+
+(function() {
+    //****************************
+    //* Write critical code here *
+    //****************************
 
 
     //Take the raw HTML health and make it into something useful.
-    var HTMLHealthString = document.getElementById(HTMLHealth).innerHTML.replace(/\s+/, "");
-    var split1 = HTMLHealthString.split("/");
-    var split2 = split1[1].split("HP");
-    var PlayerHealth = split1[0];
-    var PlayerHealthMax = split2[0];
+    var split = document.getElementById(HTMLHealth).innerHTML.replace(/\s+/, "").split("/");
+    var PlayerHealth = split[0];
+    var PlayerHealthMax = split[1].split("HP")[0];
     var PlayerHealthMin = PlayerHealthMax * 0.75;
-
 
     //Go into regen mode if PlayerHealth is less or equal than min. health
     if (PlayerHealth <= PlayerHealthMin)
     {
-        isRegenerating = true;
+        log("Currently regenerating health.");
+        log("Deired health: " + PlayerHealthMin);
+        log("Current health: " + PlayerHealth);
     }
     //Go into attack mode if PlayerHealth is more or equal than max. health
     else if (PlayerHealth >= PlayerHealthMax)
     {
-        isRegenerating = false;
-    }
-
-
-    if (isRegenerating)
-    {
-        console.log("Currently regenerating health.");
-        console.log("Deired health: " + PlayerHealthMin);
-        console.log("Current health: " + PlayerHealth);
-    }
-
-    else
-    {
+        log("Currently attacking.");
         document.getElementById(HTMLAttack).click();
     }
+    //Something went wrong report it.
+    else
+    {
+        log("Something went wrong");
+        log("Current Health: " + PlayerHealth);
+        log("Desired health: " + PlayerHealthMin);
+        log("Maximum health: " + PlayerHealthMax);
+    }
 
 
-
-        setTimeout(function () {
-            location.reload();
+    //Reloads the website after x secounds.
+    setTimeout(function () {
+        location.reload();
     }, 15 * 1000);
-    
+
+    //******************************
+    //* Write uncritical code here *
+    //******************************
+
     //The site killed the alert, so we are adding it back in here:
-    const AlertHTML = "<a class=\"alert\" href=\"#\">Shitty Alert!</a>";
     var AlertDIV = document.createElement('DIV');
     AlertDIV.className = HTMLAlert;
     AlertDIV.innerHTML = AlertHTML;
@@ -81,5 +111,15 @@
 
     //Take a random text in responses and show it in the top of the website.
     document.getElementsByClassName(HTMLAlert)[0].innerHTML = responses[Math.floor(Math.random()*responses.length)];
-    
+
+
+    //Print the console log on screen. Should run last.
+    ConsoleHTML = document.createElement("DIV");
+    ConsoleHTML.style.position = "fixed";
+    ConsoleHTML.style.bottom = "0";
+    ConsoleHTML.style.right = "0";
+    ConsoleHTML.style.margin = "10px";
+    ConsoleHTML.innerHTML = logString;
+
+    document.body.appendChild(ConsoleHTML);
 })();
