288cb6892b035787385007983ff7537c97b8e3f9
Mads Mogensen <lort256c@gmail.com>
2017-03-09 21:21:11 +0100

    Added comments

--- SocialFightBot-Stats.user.js
+++ SocialFightBot-Stats.user.js
@@ -11,29 +11,31 @@
 
 (function()
 {
+    //Get the profile url.
     var StatsURL = document.body.getElementsByClassName("girl2")[0].children[0].href;
+
+    //Make a div where the stats should be in.
     var StatsDiv = document.createElement("div");
     StatsDiv.style.position = "fixed";
     StatsDiv.style.left = "0";
+    StatsDiv.style.top = "0";
     StatsDiv.style.bottom = "0";
     StatsDiv.style.margin = "10px";
+    StatsDiv.style.marginTop = "50px";
     StatsDiv.style.marginBottom = "50px";
     StatsDiv.id = "statsdiv";
     document.body.appendChild(StatsDiv);
-
+    //Load the stats from StatsURL
     $( "#statsdiv" ).load( StatsURL + " .stats" );
     $("<div>").load(StatsURL + " .weapon", function() {
       $("#statsdiv").append($(this));
-});
+    });
     $("<div>").load(StatsURL + " .girl", function() {
       $("#statsdiv").append($(this));
+            //Delete unneeded items
             document.body.getElementsByClassName("buttons")[0].outerHTML = "";
             document.body.getElementsByClassName("girl")[0].children[0].innerHTML = "";
+            //Remove minHeight;
             document.body.getElementsByClassName("girl")[0].style.minHeight = "0";
-});
-
-    
-
-
-
+    });
 })();
